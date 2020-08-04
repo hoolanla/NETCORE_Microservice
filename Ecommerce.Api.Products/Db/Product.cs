7b694b85cf98bebcbdec986011e3b945bb9751f9
Hoolanla <prakasit.y@gmail.com>
2020-08-04 14:30:56 +0700

    Initial commit

---
+++ Ecommerce.Api.Products/Db/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Api.Products.Db
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int inventory { get; set; }
+    }
+}
---
+++ Ecommerce.Api.Products/Db/ProductsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace
+    Ecommerce.Api.Products.Db
+{
+    public class ProductsDbContext : DbContext
+    {
+
+
+    }
+
+}
